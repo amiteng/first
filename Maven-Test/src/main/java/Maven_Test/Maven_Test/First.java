e136ba032c75fd9dd0d4c0a967afcc0eca510401
Amit Kumar <amitkumar.sharma@hotmail.com>
2018-06-07 15:49:16 +0530

    My first Maven Project

---
+++ Maven-Test/src/main/java/Maven_Test/Maven_Test/First.java
@@ -0,0 +1,21 @@
+package Maven_Test.Maven_Test;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class First {
+
+	@Test
+	
+	public void check() {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.gecko.driver","D:/amit/geckodriver-v0.20.1-win32/geckodriver.exe");
+		
+		FirefoxDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.google.com");
+
+	}
+
+}
